c2121443ca921630da58ef61a36d214fa37fe62e
Peter Peng <yser.pc@gmail.com>
2021-11-11 22:31:32 -0500

    db query change for auth

--- toothflexService.js
+++ toothflexService.js
@@ -77,7 +77,7 @@ function readUsers(req, res, next) {
 }
 
 function authenticateUser(req, res, next) {
-    db.many("SELECT * FROM Users WHERE email=${email} AND password=${password}", req.body)
+    db.one("SELECT * FROM Users WHERE email=${email} AND password=${password}", req.body)
         .then(data => {
             returnDataOr404(res, data);
         })
